17d0b890ebcdcc95a4bc11a883ac566bfb680ddd
Anh Nam <nam.nguyen-anh@banvien.com.vn>
2021-10-20 00:22:33 +0700

    [Config] project

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Home } from 'pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default function RouteApp() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={<Home />} />
+      </Switch>
+    </Router>
+  );
+}
